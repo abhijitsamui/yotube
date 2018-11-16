f1df31893482b23cf6c67f6506e82dd55443fe97
Abhijit Samui <abhijit.samui@gmail.com>
2018-11-15 17:31:14 -0700

    Initial setup with Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    //TODO: callback from parent component
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui semantic">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
