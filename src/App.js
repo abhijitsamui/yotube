df61c63c60671b72d381eee5720ad2dd16fb82dc
Abhijit Samui <abhijit.samui@gmail.com>
2018-11-15 19:38:30 -0700

    displaying video list items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 import Searchbar from "./components/SearchBar";
 import youtube from "./api/youtube";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 
 class App extends Component {
-  onTermSubmit = term => {
-    youtube.get("/search", {
+  state = { videos: [], selectedVideo: null };
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    //console.log("From app", video);
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <Searchbar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
